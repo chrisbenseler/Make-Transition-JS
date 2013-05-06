f4b8a45b4b3a069c1cc62c6a1e4d9fe86e9d2b25
Christian Benseler <cbenseler@gmail.com>
2013-05-06 16:47:17 -0300

    grunt tasks configured

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,20 +11,24 @@ module.exports = function(grunt) {
       '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
       ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
     // Task configuration.
+    copy : {
+     // main : {
+        files : {
+          src: ['src/<%= pkg.name %>.css'],
+          dest: 'dist/<%= pkg.name %>.css'  
+        }
+     // }
+    },
     concat: {
       options: {
-        banner: '<%= banner %>',
-        stripBanners: true
+        
       },
       dist: {
-        src: ['lib/<%= pkg.name %>.js'],
+        src: ['src/<%= pkg.name %>.js'],
         dest: 'dist/<%= pkg.name %>.js'
       }
     },
     uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
       dist: {
         src: '<%= concat.dist.dest %>',
         dest: 'dist/<%= pkg.name %>.min.js'
@@ -71,11 +75,11 @@ module.exports = function(grunt) {
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'copy', 'concat', 'uglify']);
 
 };
