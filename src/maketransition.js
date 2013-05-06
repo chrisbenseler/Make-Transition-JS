a6afde0ee4196361fd0a5dfe466381ca50bed4ea
Christian Benseler <cbenseler@gmail.com>
2013-05-06 12:42:09 -0300

    first commit: grunt + src js

---
+++ src/maketransition.js
@@ -0,0 +1,47 @@
+var MakeTransition = function(custom_options) {
+
+	if(custom_options.element===null || custom_options.element.length==0)
+		return false;
+
+	var options = {
+		class_name : "",
+		timeout: 5000
+	};
+
+	//override default options with custom
+	for(var key in custom_options) {
+		if(options.hasOwnProperty(key)) {
+			options[key] = custom_options[key];
+		}
+	}
+
+	var slides = custom_options.element.getElementsByTagName("li");
+	var size = slides.length;
+	var current = -1;
+
+	var el = $(custom_options.element);
+	//force overflow to hidden
+	el.css("overflow", "hidden");
+
+	(navigate = function() {
+		
+		current++;
+		
+		if(current<size) {
+			$(slides[current]).addClass(options.class_name);
+			$(slides[current - 1]).removeClass(options.class_name);	
+		} else {
+			current = 0;
+			$(slides[current]).addClass(options.class_name);
+			$(slides[size - 1]).removeClass(options.class_name);	
+		}
+
+		if($(slides[current]).data("bgcolor")) {
+			el.css("background-color", $(slides[current]).data("bgcolor"));
+		}
+
+		setTimeout(function() {
+			navigate();
+		}, options.timeout);
+	}).call();
+};
